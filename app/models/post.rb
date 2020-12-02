1ee198a8d2a32af9030887a39707211ff5eec5fc
Elbert Corniell <elberthcorniell@gmail.com>
2020-12-02 16:46:53 -0400

    Rubocop offences fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-    validates :title, :body, :author_id, presence:true
-    validates :title, uniqueness: true
+  validates :title, :body, :author_id, presence: true
+  validates :title, uniqueness: true
 
-    belongs_to :author
-    has_many :comment
+  belongs_to :author
+  has_many :comment
 end
